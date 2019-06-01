9a41090808816531ee909a6f2676db6642c2fe72
TrustLak <lak.mohd@gmail.com>
2019-06-01 16:33:21 +0300

    bug fixes

--- particle_filter.cpp
+++ particle_filter.cpp
@@ -1,10 +1,17 @@
-#include <random>
+
+#include <math.h>
 #include <algorithm>
 #include <iostream>
-#include <numeric>
-#include <map>
-#include <string>
 #include <iterator>
+#include <numeric>
+#include <random>
+#include <string>
+#include <vector>
+
+#include "helper_functions.h"
+
+using std::string;
+using std::vector;
 
 #include "particle_filter.h"
 using namespace std;
@@ -16,26 +23,51 @@ ParticleFilter::ParticleFilter(){
 
 }
 
-ParticleFilter::ParticleFilter(double x, double y, double theta, double std[], double std_landmark[], int n_particles){
-    init(x, y, theta, std, std_landmark, n_particles);
+ParticleFilter::ParticleFilter(double x, double y, double theta, double std[], 
+                       double std_landmark[], int n_particles, const Map &map_landmarks, double sensor_range){
+    init(x, y, theta, std, std_landmark, n_particles, map_landmarks, sensor_range_);
 }
 
-void ParticleFilter::init(double x, double y, double theta, double std[], double std_landmark[], int n_particles){
+void ParticleFilter::init(double x, double y, double theta, double std[], 
+                       double std_landmark[], int n_particles, const Map &map_landmarks, double sensor_range){
 
     num_particles_ = n_particles;
     weights_.resize(num_particles_); 
     particles_.resize(num_particles_);
     new_particles_.resize(num_particles_);
-    
-    std_[0] = std[0];
-    std_[1] = std[1];
-    std_[2] = std[2];
+
+    std_pos_[0] = std[0];
+    std_pos_[1] = std[1];
+    std_pos_[2] = std[2];
 
     std_landmark_[0] = std_landmark[0]; 
     std_landmark_[1] = std_landmark[1];
 
-    // TODO: fix random seed
-    cout << "Fix random seed" << endl;
+    /*I
+    normal_distribution<double> x_new(0, std_pos_[0]);
+    normal_distribution<double> y_new(0, std_pos_[1]);
+    normal_distribution<double> theta_new(0, std_pos_[2]);
+
+    x_noise_ = x_new;
+    y_noise_ = y_new;
+    theta_noise_ = theta_new;
+    // fix random seed
+    //engine_.seed(42);
+    */
+    std::random_device rd{};
+    std::mt19937 engine_{rd()};
+    normal_distribution<double> x_noise_(0, std_pos_[0]);
+    normal_distribution<double> y_noise_(0, std_pos_[1]);
+    normal_distribution<double> theta_noise_(0, std_pos_[2]);
+
+
+    gauss_norm_ = 1 / (2 * M_PI * std_landmark_[0] * std_landmark_[1]);  // can remove std_landmark from here
+    norm_x_ = 2 * std_landmark_[0] * std_landmark_[0];
+    norm_y_ = 2 * std_landmark_[1] * std_landmark_[1];
+
+    landmarks_ = map_landmarks.landmark_list;    
+    sensor_range_ = sensor_range;
+    //default_random_engine engine_;
 
     for (int i = 0; i < num_particles_; i++){
         // initialize particles from normal distributions
@@ -50,16 +82,25 @@ void ParticleFilter::init(double x, double y, double theta, double std[], double
     }
 
     is_initialized_ = true;
-
+    //resetDistributions();
 }
 
-void ParticleFilter::predict(double delta_t, double velocity, double yaw_rate){
+void ParticleFilter::prediction(double delta_t, double velocity, double yaw_rate){
     // loop over each particle and update the position based on vel and yaw_rate
+    //random_device rand_dev_;
+    //default_random_engine engine_;
+
+    std::random_device rd{};
+    std::mt19937 engine_{rd()};
+    normal_distribution<double> x_noise_(0, std_pos_[0]);
+    normal_distribution<double> y_noise_(0, std_pos_[1]);
+    normal_distribution<double> theta_noise_(0, std_pos_[2]);
+
     for (int i = 0; i < num_particles_; i++){
-        if (yaw_rate == 0){
+        if (fabs(yaw_rate) == 0){
             particles_[i].x += x_noise_(engine_) + velocity * delta_t * cos(particles_[i].theta);
             particles_[i].y += y_noise_(engine_) + velocity * delta_t * sin(particles_[i].theta);
-            particles_[i].theta += theta_noise_(engine_);
+            particles_[i].theta += theta_noise_(engine_) //+ yaw_rate * delta_t;
         }
         else {
             particles_[i].x += x_noise_(engine_) + 
@@ -68,29 +109,60 @@ void ParticleFilter::predict(double delta_t, double velocity, double yaw_rate){
                 (velocity/yaw_rate) * (cos(particles_[i].theta) - cos(particles_[i].theta + (yaw_rate * delta_t)));
             particles_[i].theta += theta_noise_(engine_) + yaw_rate * delta_t;
         }
+
     }
+    //resetDistributions();
 }
 
-void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
-                            const vector<LandmarkObs> &observations, const Map &map_landmarks){
+void ParticleFilter::updateWeights(const vector<LandmarkObs> &observations){
 
-    return;    
-    
+    for (int i = 0; i < num_particles_; i++){
+        double pi_gauss = 1.0;  // 
+
+        for (unsigned int j = 0; j < observations.size(); j++){
+            // vehicle to map coordinates
+            double x_map = observations[j].x * cos(particles_[i].theta) - observations[j].y * sin(particles_[i].theta) + particles_[i].x;
+            double y_map = observations[j].y * sin(particles_[i].theta) + observations[j].y * cos(particles_[i].theta) + particles_[i].y;
+
+            // landmarks within sensor range
+            vector<double> delta_distances; // landmark-observation mean values; assume very large value initially  
+            for (unsigned int k = 0; k < landmarks_.size(); k++){
+                // square euclidean distance 
+                double particle_dist = pow(particles_[i].x - landmarks_[k].x_f, 2) + pow(particles_[i].y - landmarks_[k].y_f, 2);
+                if (particle_dist <= sensor_range_*sensor_range_){
+                    delta_distances.push_back( pow(x_map - landmarks_[k].x_f, 2) + pow(y_map - landmarks_[k].y_f, 2) );
+                }
+            }
+            // associate with nearest landmark 
+            int min_index = distance(delta_distances.begin(), min_element(delta_distances.begin(),delta_distances.end()));
+
+            // update the guassian (product of guassians)
+            double exponent  = pow(x_map - landmarks_[min_index].x_f, 2) / norm_x_ 
+                             + pow(y_map - landmarks_[min_index].y_f, 2) / norm_y_;
+            pi_gauss *= gauss_norm_ * exp(-exponent);
+        }
+        particles_[i].weight = pi_gauss;
+        weights_[i] = particles_[i].weight;
+
+    }
 }
 
 
 void ParticleFilter::resample(){
     
+        std::random_device rd{};
+        std::mt19937 engine_{rd()};
+
+
     discrete_distribution<int> idx(weights_.begin(), weights_.end());
     for (int i = 0; i < num_particles_; i++){
         new_particles_[i] = particles_[idx(engine_)];
     }
-
     particles_ = new_particles_; // no need to worry about shallow copy
 }
 
 
-Particle ParticleFilter::SetAssociations(Particle& particle, 
+void ParticleFilter::SetAssociations(Particle& particle, 
                                      const vector<int>& associations, 
                                      const vector<double>& sense_x, 
                                      const vector<double>& sense_y) {
@@ -109,6 +181,7 @@ string ParticleFilter::getAssociations(Particle best) {
     return s;
 }
 
+
 string ParticleFilter::getSenseCoord(Particle best, string coord) {
     vector<double> v;
 
@@ -124,3 +197,18 @@ string ParticleFilter::getSenseCoord(Particle best, string coord) {
     s = s.substr(0, s.length()-1);  // get rid of the trailing space
     return s;
 }
+
+
+vector<Particle> ParticleFilter::getParticles(){
+    return particles_;
+}
+
+/*
+void ParticleFilter::resetDistributions(){
+    x_noise_.reset();
+    y_noise_.reset();
+    theta_noise_.reset();
+}
+*/
+
+
--- particle_filter.h
+++ particle_filter.h
@@ -1,7 +1,7 @@
 #ifndef PARTICLE_FILTER_H_
 #define PARTICLE_FILTER_H_
 
-#include "helper_function.h"
+#include "helper_functions.h"
 
 #include <string>
 #include <random>
@@ -24,40 +24,44 @@ struct Particle {
 class ParticleFilter{
     private:
         int num_particles_;
-        double std_[3];
-        double std_landmark_[2]; 
+        double std_pos_[3];
+        double std_landmark_[2]; // we can remove this 
+        double gauss_norm_;
+        double norm_x_;
+        double norm_y_; 
         vector<double> weights_;
         vector<Particle> particles_;
         vector<Particle> new_particles_;
+        vector<Map::single_landmark_s> landmarks_;
+        double sensor_range_;
 
         // add sampling engine to avoid reinitalizing everytime
-        random_device rand_dev_{};
-        default_random_engine engine_{rand_dev_()};
-        normal_distribution<double> x_noise_;
-        normal_distribution<double> y_noise_;
-        normal_distribution<double> theta_noise_;
+
 
         bool is_initialized_;
 
     public:
         ParticleFilter();
-        ParticleFilter(double x, double y, double theta, double std[], double std_landmark[], int n_particles);
+        ParticleFilter(double x, double y, double theta, double std[], 
+                       double std_landmark[], int n_particles, const Map &map_landmarks, double sensor_range);
         // ~ParticleFilter();   TODO: delete pointers
-        void init(double x, double y, double theta, double std[], double std_landmark[], int n_particles);
-        void predict(double delta_t, double velocity, double yaw_rate);  // need not use std_pos here...
+        void init(double x, double y, double theta, double std[], 
+                  double std_landmark[], int n_particles, const Map &map_landmarks, double sensor_range);
+        void prediction(double delta_t, double velocity, double yaw_rate);  // need not use std_pos here...
         void dataAssociation(vector<LandmarkObs> predicted, vector<LandmarkObs>& observations);
-        void updateWeights(double sensor_range, double std_landmark[], 
-                            const vector<LandmarkObs> &observations, const Map &map_landmarks);
+        void updateWeights(const vector<LandmarkObs> &observations);
         void resample();
-        Particle SetAssociations(Particle& particle, 
+        void SetAssociations(Particle& particle, 
                                      const vector<int>& associations, 
                                      const vector<double>& sense_x, 
                                      const vector<double>& sense_y);
 
         string getAssociations(Particle best);
         string getSenseCoord(Particle best, string coord);
-        string getSenseX(Particle best);
-        string getSenseY(Particle best);
+
         const bool initialized() const { return is_initialized_;}
+        vector<Particle> getParticles(); // needed for grading...
+        //void resetDistributions(); // reset normal distributions
 };
 #endif
+
